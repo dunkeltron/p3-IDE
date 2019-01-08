0d745d5e20c3747f699e29c15fa3082ac73f3b9d
Ian Dunkelberg <idunkelberg@gmail.com>
2019-01-08 00:10:51 -0800

    Changed text in Nav component. fixed .gitignore not ignoring node_modules.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -13,13 +13,13 @@ function Nav() {
       
         <li className="nav-item   dropdown  ">
           <a className="nav-link dropdown-toggle  " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Account
+            UserName
           </a>
           <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item" href="#">Action</a>
-            <a className="dropdown-item" href="#">Another action</a>
+            <a className="dropdown-item" href="#">Account</a>
+            <a className="dropdown-item" href="#">Other Projects</a>
             <div className="dropdown-divider"></div>
-            <a className="dropdown-item" href="#">Something else here</a>
+            <a className="dropdown-item" href="#">Log Out</a>
           </div>
         </li>
       
