a369b8320b9f3e8e0746456c873851cb4adc18fe
Ian Dunkelberg <idunkelberg@gmail.com>
2019-01-12 14:13:06 -0800

    fixed styling issues with editor pane due to min-height being set from node-modules. added comments for further development suggestions to Editor and EditorContainer.

--- client/src/components/Editor/index.js
+++ client/src/components/Editor/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import{UnControlled as CodeMirror} from 'react-codemirror2';
 require('codemirror/lib/codemirror.css');
 require('codemirror/theme/material.css');
@@ -8,11 +7,12 @@ require('codemirror/mode/xml/xml.js');
 require('codemirror/mode/javascript/javascript.js');
 require('codemirror/mode/css/css.js');
 require('codemirror/mode/htmlmixed/htmlmixed.js');
+require( "./editor.css");
 function Editor(props) {
   return (
     <div className="col-md-6 border border-secondary mh-25 editor">
         <CodeMirror
-            value='<h1>I ♥ react-codemirror2</h1>'
+            value='<h1>I ♥ react-codemirror2</h1>'//this.state.code
             options={{
                 mode: props.lang,
                 theme: 'material',
--- client/src/pages/EditorContainer.js
+++ client/src/pages/EditorContainer.js
@@ -3,20 +3,21 @@ import Editor from "../components/Editor";
 
 import Nav from "../components/Nav";
 function EditorContainer() {
+    //g
     return (
         <div>
-            
+            {/* remember to mess with .editor class in codemirror */}
         <Nav/>
         <div className="container editor-container ">
             <div className="col projects">
             </div>
             <div className="col">
                 <div className = "row top-row mh-25">
-                    <Editor lang ="javascript"/>
-                    <Editor lang ="css"/>
+                    <Editor lang ="javascript"/> {/* add code prop*/}
+                    <Editor lang ="css"/> {/* add code prop*/}
                 </div>
                 <div className ="row bottom-row mh-25">                
-                    <Editor lang ="htmlmixed"/>
+                    <Editor lang ="htmlmixed"/> {/* add code prop*/}
                     <iframe className="render-window mh-25 md-6" title="Render Panel"></iframe>
                 </div>
             </div>
