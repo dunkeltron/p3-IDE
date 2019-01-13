013596646dba97a3a2162796edbbb3002108474f
Ian Dunkelberg <idunkelberg@gmail.com>
2019-01-13 14:39:40 -0800

    initializing dev branch from master.

---
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+function ProjectCard(props) {
+  return (
+    <div className="col-3 border border-secondary" id={this.props._id}>
+        <span className="project-title" >Project</span>
+    </div>
+  );
+
+
+}
+
+
+export default ProjectCard;
