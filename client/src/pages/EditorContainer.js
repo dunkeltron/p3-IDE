638f3554cc5dfde7f7e4c033a864122b5de5cf3b
Ian Dunkelberg <idunkelberg@gmail.com>
2019-01-13 16:17:11 -0800

    changing paths around to match github / codepen projects / user routing in app.js. added background image in index.css. Added console.logs to editor to test pathing changes. Profile page now renders users Username. started work on LogInCard component to be used in LogIn.js.

--- client/src/pages/EditorContainer.js
+++ client/src/pages/EditorContainer.js
@@ -1,32 +1,51 @@
-import React from "react";
+import React, {Component} from "react";
 import Editor from "../components/Editor";
 
 import Nav from "../components/Nav";
-function EditorContainer() {
-    //g
-    return (
-        <div>
-            {/* remember to mess with .editor class in codemirror */}
-        <Nav/>
-        <div className="container editor-container ">
-            <div className="col projects">
-            </div>
-            <div className="col">
-                <div className = "row top-row mh-25">
-                    <Editor lang ="javascript"/> {/* add code prop*/}
-                    <Editor lang ="css"/> {/* add code prop*/}
-                </div>
-                <div className ="row bottom-row mh-25">                
-                    <Editor lang ="htmlmixed"/> {/* add code prop*/}
-                    <iframe className="render-window mh-25 md-6" title="Render Panel"></iframe>
-                </div>
-            </div>
-        </div>
+class EditorContainer extends Component{
+    state={
+        project:{}
+    }
+    componentDidMount(){
+        const {user,id} = this.props.match.params;
+        if(id){
+            console.log(user,id);
+        }
+        else{
+            console.log("no id");
+        }
         
-        </div>
+        
+    }
+    render(){
+
+        return (
+            <div>
+                {/* remember to mess with .editor class in codemirror */}
+            <Nav/>
+            <div className="container editor-container ">
+            
+                <div className="col projects">
+                </div>
+                <div className="col">
+                    <div className = "row top-row mh-25">
+                        <Editor lang ="javascript"/> {/* add code prop*/}
+                        <Editor lang ="css"/> {/* add code prop*/}
+                    </div>
+                    <div className ="row bottom-row mh-25">                
+                        <Editor lang ="htmlmixed"/> {/* add code prop*/}
+                        <iframe className="render-window mh-25 md-6" title="Render Panel"></iframe>
+                    </div>
+                </div>
+            </div>
+            
+            </div>
+        
+        );
     
-    );
+    }
     
 }
+
 
 export default EditorContainer
--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,7 +1,9 @@
 
 import React, { Component } from "react";
+
+
 class LogIn extends Component {
-    
+  
   //will have to change function once authentication is up
     handleSubmit = (event) => {
         event.preventDefault();
@@ -9,24 +11,27 @@ class LogIn extends Component {
     }
     render(){
     return (
-        <div class="container justify-content-center">
-        <div class="card card-container col-4 mx-auto">
-            <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
-            <p id="profile-name" class="profile-name-card"></p>
-            <form class="form-signin">
-                <input type="username" id="inputUsername" class="form-control" placeholder="Username" required />
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required/> 
+        <div className="container mt-5 justify-content-center">
+        <div className="card card-container col-md-4 col-xs-12 col-xl-4 mx-auto">
+            <img id="profile-img" className="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+            <p id="profile-name" className="profile-name-card text-center">Brand Image</p>
+            <form className="form-signin">
+                <input type="username" id="inputUsername" className="form-control" placeholder="Username" required />
+                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/> 
                 {/* potentially  nice feature to have.
-                <div id="remember" class="checkbox">
+                <div id="remember" className="checkbox">
                     <label>
                         <input type="checkbox" value="remember-me"> Remember me</input>
                     </label>
                 </div> */}
-                <a class="btn btn-lg btn-primary btn-block btn-signin"  href="/profile">Sign in</a>
+                {/* the hrefs for these buttons will have to be changed once user authentication is added */}
+                <a className="btn  btn-secondary btn-block btn-signin"  href="/editor/33">Sign in</a>
+                <a className="btn  btn-secondary btn-block btn-signin"  href="/profile">Register</a>
             </form>
-            <a href="/editor" class="forgot-password">
+            {/* If we get to this point we'll have to create a page or form for this. 
+            <a  href ="/forgotpassword" className="forgot-password">
                 Forgot your password?
-            </a>
+            </a> */}
          </div>{/*<!-- /card-container --> */}
     </div>
     
