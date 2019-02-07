83aa50619d7b708b9ca9f671a6e80be893af78d4
tdar31 <t.dardani8@gmail.com>
2019-02-06 18:13:46 -0800

    images added on profile page. particlesjs setup but freaks out a bit when you update the state

--- client/src/components/SocialLinks/index.js
+++ client/src/components/SocialLinks/index.js
@@ -6,28 +6,28 @@ function SocialLinks({ user, srcGith, srcLinked, srcPersonal }) {
     <div className="SLContainer">
       <a
         className="imageAnchor socialLinks"
-        href={srcGith||"www.github.com"}
+        href={srcGith || "www.github.com"}
         target="_blank"
         rel="noopener noreferrer"
       >
         <div className="gitText">
           <img
-            src="https://via.placeholder.com/90"
+            src="https://i.ibb.co/9GQcnYh/Background.png"
             className="gitAva"
             alt="github logo"
           />
         </div>
       </a>
-
+      
       <a
         className="imageAnchor socialLinks"
-        href={srcLinked||"www.linkedin.com"}
+        href={srcLinked || "www.linkedin.com"}
         target="_blank"
         rel="noopener noreferrer"
       >
         <div className="linkedinText">
           <img
-            src="https://via.placeholder.com/90"
+            src="https://i.ibb.co/80Zwxz9/linkedin-Logo.png"
             className="linkedinAva"
             alt="linkedin logo"
           />
@@ -36,13 +36,13 @@ function SocialLinks({ user, srcGith, srcLinked, srcPersonal }) {
 
       <a
         className="imageAnchor socialLinks"
-        href={srcPersonal||null}
+        href={srcPersonal || null}
         target="_blank"
         rel="noopener noreferrer"
       >
         <div className="personalText">
           <img
-            src="https://via.placeholder.com/90"
+            src="https://i.ibb.co/fSc2Hp7/personal-Addres.png"
             className="personalAva"
             alt="personal logo"
           />
@@ -53,3 +53,8 @@ function SocialLinks({ user, srcGith, srcLinked, srcPersonal }) {
 }
 
 export default SocialLinks;
+
+//Don't remove just incase links are lost for whatever reason
+//<a href="https://imgbb.com/"><img src="https://i.ibb.co/80Zwxz9/linkedin-Logo.png" alt="linkedin-Logo" border="0"></a>
+//<a href="https://imgbb.com/"><img src="https://i.ibb.co/fSc2Hp7/personal-Addres.png" alt="personal-Addres" border="0"></a>
+//<a href="https://imgbb.com/"><img src="https://i.ibb.co/9GQcnYh/Background.png" alt="Background" border="0"></a>
--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,68 +1,141 @@
-
 import React, { Component } from "react";
 import LogInForm from "../components/LogInForm";
-import { Redirect} from 'react-router-dom';
-
+import { Redirect } from "react-router-dom";
+import Particles from "react-particles-js";
 
 class LogIn extends Component {
-    constructor(){
-
+  constructor() {
     super();
-    this.state={
-        username:"",
-        password:"",
-        currentUser: null,
-        redirectTo: null
+    this.state = {
+      username: "",
+      password: "",
+      currentUser: null,
+      redirectTo: null
     };
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
-}
- 
+  }
+
   //will have to change function once authentication is up
-    handleFormSubmit = (event) => {
-        //attempt to login using api call from App.js and current state of email and password.
-        event.preventDefault();
-        console.log(this.state);
-        this.props._login(this.state.username,this.state.password);
-        this.setState({redirectTo: this.state.username});
- 
+  handleFormSubmit = event => {
+    //attempt to login using api call from App.js and current state of email and password.
+    event.preventDefault();
+    console.log(this.state);
+    this.props._login(this.state.username, this.state.password);
+    this.setState({ redirectTo: this.state.username });
+  };
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  componentWillMount() {
+    sessionStorage.removeItem("currentUser");
+  }
+  render() {
+    // this handles the redirect after login by checking if we have assigned redirectTo
+    if (this.state.redirectTo) {
+      return <Redirect to={{ pathname: this.state.redirectTo }} />;
+    } else {
+      return (
+        <div>
+          <Particles className="particles-js"
+            params={{
+              particles: {
+                number: {
+                  value: 58,
+                  density: { enable: true, value_area: 800 }
+                },
+                color: { value: "#ebe6e6" },
+                shape: {
+                  type: "circle",
+                  stroke: { width: 2, color: "#000000" },
+                  polygon: { nb_sides: 7 },
+                //   image: { src: "img/github.svg", width: 100, height: 100 }
+                },
+                opacity: {
+                  value: 0.21646062821684559,
+                  random: false,
+                  anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false
+                  }
+                },
+                size: {
+                  value: 40.08530152163807,
+                  random: true
+                },
+                line_linked: {
+                  enable: false,
+                  distance: 150,
+                  color: "#ffffff",
+                  opacity: 0.4,
+                  width: 1
+                },
+                move: {
+                  enable: true,
+                  speed: 5,
+                  direction: "top-right",
+                  random: false,
+                  straight: false,
+                  out_mode: "out",
+                  bounce: false,
+                  attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 641.3648243462092
+                  }
+                }
+              },
+              interactivity: {
+                detect_on: "window",
+                events: {
+                  onhover: { enable: true, mode: "bubble" },
+                //   onclick: { enable: false, mode: "push" },
+                  resize: true
+                },
+                modes: {
+                  grab: { distance: 400, line_linked: { opacity: 1 } },
+                  bubble: {
+                    distance: 400,
+                    size: 40,
+                    duration: 2,
+                    opacity: 8,
+                    speed: 3
+                  },
+                  repulse: { distance: 200, duration: 0.4 },
+                  push: { particles_nb: 4 },
+                  remove: { particles_nb: 2 }
+                }
+              }
+            }}
+          />
+
+          <div className="row no-gutters">
+            <div className="col-md-6 m-auto">
+              <div className="card card-body mt-5">
+                <h1 className="text-center mb-3">
+                  <i className="fas fa-door-open" /> Login
+                </h1>
+                <LogInForm
+                  handleFormSubmit={this.handleFormSubmit}
+                  handleInputChange={this.handleInputChange}
+                />
+
+                <p className="lead mt-4">
+                  Need an Account?
+                  <a href="/register">Register Here</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
     }
-    handleInputChange = (event) => {
-        const {name,value} = event.target;
-        this.setState({
-            [name]:value
-        })
-    }
-    componentWillMount(){
-        sessionStorage.removeItem("currentUser");
-    }
-    render(){
-        // this handles the redirect after login by checking if we have assigned redirectTo 
-        if(this.state.redirectTo){
-            return <Redirect to={{ pathname: this.state.redirectTo}}/>
-        }
-        else{
-            return (
-                <div className="row mt-5">
-                    <div className="col-md-6 m-auto">
-                        <div className="card card-body">
-                            <h1 className="text-center mb-3">
-                                <i className="fas fa-door-open"></i> Login
-                            </h1>
-                            <LogInForm handleFormSubmit={this.handleFormSubmit} handleInputChange ={this.handleInputChange}/>
-                        
-                            <p className="lead mt-4">
-                                Need an Account?  
-                                <a href="/register">Register Here</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            
-            );
-        }
-    }
-    
+  }
 }
 
 export default LogIn;
